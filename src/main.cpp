79f99190d5727d3f674aeef8faa27931bd06a2c9
Neil Stevens <neil@stevensmedia.us>
2022-07-09 12:35:32 -0400

    Better correctly set up the UI for MainWindow

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
 #include <QApplication>
 
 #include "MainWindow.h"
-#include "ui_MainWindow.h"
 
 int main(int argc, char **argv) {
 	QApplication app(argc, argv);
 	MainWindow mainWindow;
 	mainWindow.show();
-
 	return app.exec();
 }
